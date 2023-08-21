cdc3f707b3bb08d8a98a078700f4fc281f38c385
Tomsnhl <thomasdrots@outlook.fr>
2023-08-21 19:08:14 +0200

    ServiceController et ControllerMessage (complet)

--- sport_live_back/src/Repository/UserRepository.php
+++ sport_live_back/src/Repository/UserRepository.php
@@ -2,26 +2,26 @@
 
 namespace App\Repository;
 
-use App\Entity\USER;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<USER>
+ * @extends ServiceEntityRepository<User>
  *
- * @method USER|null find($id, $lockMode = null, $lockVersion = null)
- * @method USER|null findOneBy(array $criteria, array $orderBy = null)
- * @method USER[]    findAll()
- * @method USER[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class USERRepository extends ServiceEntityRepository
+class UserRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, USER::class);
+        parent::__construct($registry, User::class);
     }
 
-    public function add(USER $entity, bool $flush = false): void
+    public function add(User $entity, bool $flush = false): void
     {
         $this->getEntityManager()->persist($entity);
 
@@ -30,7 +30,7 @@ class USERRepository extends ServiceEntityRepository
         }
     }
 
-    public function remove(USER $entity, bool $flush = false): void
+    public function remove(User $entity, bool $flush = false): void
     {
         $this->getEntityManager()->remove($entity);
 
@@ -40,7 +40,7 @@ class USERRepository extends ServiceEntityRepository
     }
 
 //    /**
-//     * @return USER[] Returns an array of USER objects
+//     * @return User[] Returns an array of User objects
 //     */
 //    public function findByExampleField($value): array
 //    {
@@ -54,7 +54,7 @@ class USERRepository extends ServiceEntityRepository
 //        ;
 //    }
 
-//    public function findOneBySomeField($value): ?USER
+//    public function findOneBySomeField($value): ?User
 //    {
 //        return $this->createQueryBuilder('u')
 //            ->andWhere('u.exampleField = :val')
