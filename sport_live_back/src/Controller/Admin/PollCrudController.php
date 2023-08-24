f92864a21b8e822efb814498cd1bdf3b49c1e523
Tomsnhl <thomasdrots@outlook.fr>
2023-08-24 09:44:27 +0200

    essaie de deploiment

--- sport_live_back/src/Controller/Admin/PollCrudController.php
+++ sport_live_back/src/Controller/Admin/PollCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Poll;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PollCrudController extends AbstractCrudController
