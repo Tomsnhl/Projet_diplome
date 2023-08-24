388fde3a86bc04552d8d4e5dba0ed3555e4c4955
Tomsnhl <thomasdrots@outlook.fr>
2023-08-24 09:53:28 +0200

    rename .env redeploiment

--- sport_live_back/src/Controller/BackOffice/PollController.php
+++ sport_live_back/src/Controller/BackOffice/PollController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Admin;
+namespace App\Controller\BackOffice;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
