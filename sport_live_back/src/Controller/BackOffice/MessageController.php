9c3d2abc6e9303826ce447e5d012939ab5cbba8f
KevinLeroyoclock <kevin.leroy@oclock.school>
2023-08-28 16:09:06 +0200

    rectification des routes de messages

--- sport_live_back/src/Controller/BackOffice/MessageController.php
+++ sport_live_back/src/Controller/BackOffice/MessageController.php
@@ -2,93 +2,118 @@
 
 namespace App\Controller\BackOffice;
 
-use DateTimeImmutable;
 use App\Entity\Message;
-use App\Form\MessageType;
+use App\Form\MessageType;  
 use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/admin/message")
+ */
+class MessageController extends AbstractController {
 
-class MessageController extends AbstractController
-{
     /**
-     * @Route("/admin/message", name="app_back_message_list", methods={"GET"})
+     * Affiche la liste de tous les sondages.
+     *
+     * @Route("/", name="admin_message_index")
+     *
+     * @param MessageRepository $MessageRepository Le repository pour accéder aux sondages.
+     *
+     * @return Response La vue affichant la liste des sondages.
      */
-    public function list(MessageRepository $messageRepository): Response
-    {
+    public function index(MessageRepository $MessageRepository): Response {
+        $message = $MessageRepository->findAll();
+
         return $this->render('back/message/list.html.twig', [
-            'messages' => $messageRepository->findAll(),
+            'messages' => $message
         ]);
     }
 
     /**
-     * @Route("/admin/message/ajouter", name="app_back_message_add", methods={"GET", "POST"})
+     * Crée un nouveau message.
+     *
+     * @Route("/new", name="admin_message_new")
+     *
+     * @param Request $request L'objet request pour gérer les requêtes HTTP.
+     * @param EntityManagerInterface $em L'entity manager pour gérer les entités.
+     *
+     * @return Response La vue pour la création de sondage ou la redirection vers la liste des sondages.
      */
-    public function add(Request $request, MessageRepository $messageRepository): Response
-    {
-        $message = new Message();
-        $message->setSentDate(new DateTimeImmutable('now'));
+    public function create(Request $request, EntityManagerInterface $em): Response {
+        $message = new message();
         $messageForm = $this->createForm(MessageType::class, $message);
         $messageForm->handleRequest($request);
 
         if ($messageForm->isSubmitted() && $messageForm->isValid()) {
+            $em->persist($message);
+            $em->flush();
 
-            $messageRepository->add($message, true);
-
-            return $this->redirectToRoute('app_admin_dashboard_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('admin_message_index');
         }
 
-        return $this->renderForm('back/message/edit.html.twig', [
-            'message' => $message,
-            'messageForm' => $messageForm,
+        return $this->render('back/message/new.html.twig', [
+            'messageForm' => $messageForm->createView()
         ]);
     }
 
     /**
-     * @Route("/admin/message/{id}", name="app_back_message_show", methods={"GET"})
+     * Affiche les détails d'un message spécifique.
+     *
+     * @Route("/{id}", name="admin_message_show")
+     *
+     * @param message $message Le sondage à afficher.
+     *
+     * @return Response La vue affichant les détails du sondage.
      */
-    public function show(Message $message): Response
-    {
+    public function show(message $message): Response {
         return $this->render('back/message/show.html.twig', [
-            'message' => $message,
+            'messages' => $message
         ]);
     }
 
     /**
-     * @Route("/admin/message/modifier/{id}", name="app_back_message_edit", methods={"GET", "POST"})
+     * Modifie un message spécifique.
+     *
+     * @Route("/{id}/edit", name="admin_message_edit")
+     *
+     * @param Request $request L'objet request pour gérer les requêtes HTTP.
+     * @param message $message Le sondage à modifier.
+     * @param EntityManagerInterface $em L'entity manager pour gérer les entités.
+     *
+     * @return Response La vue pour l'édition de sondage ou la redirection vers la liste des sondages.
      */
-    public function edit(Request $request, Message $message, MessageRepository $messageRepository): Response
-    {
-        
-        $messageForm = $this->createForm(MessageType::class, $message, ["custom_option" => "edit"]);
-
+    public function edit(Request $request, message $message, EntityManagerInterface $em): Response {
+        $messageForm = $this->createForm(MessageType::class, $message);
         $messageForm->handleRequest($request);
 
         if ($messageForm->isSubmitted() && $messageForm->isValid()) {
-            $messageRepository->add($message, true);
+            $em->flush();
 
-            return $this->redirectToRoute('app_admin_dashboard_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('admin_message_index');
         }
 
-        return $this->renderForm('back/message/edit.html.twig', [
-            'message' => $message,
-            'messageForm' => $messageForm,
+        return $this->render('back/message/edit.html.twig', [
+            'messages' => $message,
+            'messageForm' => $messageForm->createView()
         ]);
     }
 
-    /**
-     * @Route("/admin/message/supprimer/{id}", name="app_back_message_delete", methods={"POST"})
-     */
-    public function delete(Request $request, Message $message, MessageRepository $messageRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$message->getId(), $request->request->get('_token'))) {
-            $messageRepository->remove($message, true);
-        }
+   /**
+    * @Route("/{id}/delete", name="admin_message_delete", methods={"POST"})
+    */
+        public function delete(Request $request, message $message, EntityManagerInterface $em): Response {
+            if ($this->isCsrfTokenValid('delete'.$message->getId(), $request->request->get('_token'))) {
 
-        return $this->redirectToRoute('app_admin_dashboard_index', [], Response::HTTP_SEE_OTHER);
+        // Supprimer le sondage
+        $em->remove($message);
+        $em->flush();
     }
+
+    return $this->redirectToRoute('admin_message_index');
+}
+
 }
