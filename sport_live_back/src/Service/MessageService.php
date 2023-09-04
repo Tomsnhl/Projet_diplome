4daf9606835392c03faf89976453d7304177cddc
Tomsnhl <thomasdrots@outlook.fr>
2023-09-04 16:55:25 +0200

    Message DESC

--- sport_live_back/src/Service/MessageService.php
+++ sport_live_back/src/Service/MessageService.php
@@ -68,7 +68,7 @@ class MessageService
     public function getAllMessages(): array
     {
         // Utilisation de Doctrine pour récupérer tous les messages
-        return $this->managerRegistry->getRepository(Message::class)->findAll();
+        return $this->managerRegistry->getRepository(Message::class)->findBy([], ['sentDate' => 'DESC']);
     }
 
     /**
